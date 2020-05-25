db0f840a9552233557872b8b0e4bca8b5e614cbe
unknown <italo.ads31@gmail.com>
2020-05-25 16:51:28 -0300

    Melhoria no atualizar do crud. v2

--- src/main/java/br/com/projeto/ead/service/PessoaService.java
+++ src/main/java/br/com/projeto/ead/service/PessoaService.java
@@ -59,6 +59,7 @@ public class PessoaService {
     public ResponseEntity<PessoaDto> atualizar(@RequestBody PessoaDto pessoaDto, @RequestParam Long id) throws Exception {
         Pessoa pessoa = ifExist(id);
         mapper.map(pessoa,PessoaDto.class);
+        repository.save(pessoa);
         return ResponseEntity.ok(new PessoaDto(pessoa));
     }
 
