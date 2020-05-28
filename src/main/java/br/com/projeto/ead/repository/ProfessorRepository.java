23b7f8df76b70985ae75c38bc82b18d6e9bcf045
italo jose <italo.alcantara@e-deploy.com.br>
2020-05-28 13:12:42 -0300

    pessoa

--- src/main/java/br/com/projeto/ead/repository/ProfessorRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.projeto.ead.repository;
-
-import br.com.projeto.ead.model.Professor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProfessorRepository extends JpaRepository<Professor,Long> {
-}
